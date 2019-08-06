45c5216d091906b9882c6e5fc253756feb6ff6bf
2mc-j <failTo23>
2019-08-06 19:09:27 +0300

    start rerfactorig to ROUTER

---
+++ src/containers/pages/Main/ResetPasswordConfirm/ResetPasswordConfirm.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+
+import ResetPasswordConfirmCodeForm from '../../forms/ResetPasswordConfirmCodeForm/ResetPasswordConfirmCodeForm';
+
+import { Actions} from '../../../store/password';
+
+// const mapStateToProps = (state: AppState) => {
+//   return {
+//     getMeState: getMe(state),
+//     getMeError: getMeError(state)
+//   };
+// };
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  resetPasswordConfirmCode: () => dispatch(Actions.resetPasswordConfirmCodeAction.action())
+});
+
+type Props =
+  // & ReturnType<typeof mapStateToProps>
+  & ReturnType<typeof mapDispatchToProps>
+  ;
+
+
+const ResetPasswordConfirm = (props: Props) => {
+  const { resetPasswordConfirmCode } = props;
+
+  const handelOnsubmitResetConfirmCode = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    resetPasswordConfirmCode();
+  };
+
+  return (
+    <section>
+      <ResetPasswordConfirmCodeForm onSubmit={handelOnsubmitResetConfirmCode}/>
+    </section>
+  )
+};
+
+export default connect(null, mapDispatchToProps)(ResetPasswordConfirm);
