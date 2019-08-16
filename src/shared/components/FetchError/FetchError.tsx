7339355709c0105a73f83be4a8bbfe8c1b43699c
itolog <paukanlove@gmail.com>
2019-08-16 15:48:40 +0300

    FetchErrors refactoring

--- src/shared/components/FetchError/FetchError.tsx
+++ src/shared/components/FetchError/FetchError.tsx
@@ -1,22 +1,27 @@
+import { makeStyles } from '@material-ui/styles';
 import React from 'react';
 
-import './fetchError.css';
+const useStyles = makeStyles({
+  errorItems: {
+    padding: '2px',
+  },
+});
 
 interface Props {
   data: any;
 }
 
-const FetchError = (props: Props) => {
-
-  const fetchErrors = props.data && props.data.result && props.data.result.errors;
-  const validateErrors = props.data && props.data.errors;
+const FetchError: React.FC<Props> = ({ data }) => {
+  const classes = useStyles();
+  const fetchErrors = data && data.result && data.result.errors;
+  const validateErrors = data && data.errors;
 
   if (fetchErrors) {
     return (
       <>
         {fetchErrors.map((item: any, index: number) => {
           return (
-            <p key={index} className='error-block'>{item.message}</p>
+            <div key={index} className='error-block'>{item.message}</div>
           );
         })}
       </>
@@ -28,18 +33,27 @@ const FetchError = (props: Props) => {
         {validateErrors.map((item: any, index: number) => {
           return (
             <div key={index} className='error-block'>
-              <div className='error-items'>{item.fields && item.fields.email && item.fields.email.unique}</div>
+              <div className='error-items'>{
+                item.fields.hasOwnProperty('email')
+                && item.fields.email.unique
+              }</div>
 
               <div
-                className='error-items'
-              >{item.fields
-              && item.fields.isAgreeWithPrivacyPolicyAndTermOfUse
-              && item.fields.isAgreeWithPrivacyPolicyAndTermOfUse.isAgree
-              }
+                className={classes.errorItems}
+              >
+                {item.fields.hasOwnProperty('isAgreeWithPrivacyPolicyAndTermOfUse')
+                && item.fields.isAgreeWithPrivacyPolicyAndTermOfUse.isAgree
+                }
               </div>
-              <div className='error-items'>{item.fields && item.fields.phone && item.fields.phone.matches}</div>
-              <div className='error-items'>{item.fields && item.fields.password && item.fields.password.matches}</div>
-              <div className='error-items'>{item.message}</div>
+              <div className={classes.errorItems}>
+                {item.fields.hasOwnProperty('phone') && item.fields.phone.matches}
+              </div>
+              <div className={classes.errorItems}>
+                {item.fields.hasOwnProperty('password') && item.fields.password.matches}
+              </div>
+              <div className={classes.errorItems}>
+                {item.message}
+              </div>
             </div>
           );
         })}
@@ -48,7 +62,6 @@ const FetchError = (props: Props) => {
   } else {
     return null;
   }
-
 };
 
 export default FetchError;
